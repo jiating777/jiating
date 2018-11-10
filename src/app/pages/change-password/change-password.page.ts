0274921b23ec896e0383e00b09a6728280a732bf
jiating <jiating@shopexunit.com>
2018-11-10 20:40:13 +0800

    第五次

--- src/app/pages/change-password/change-password.page.ts
+++ src/app/pages/change-password/change-password.page.ts
@@ -3,6 +3,7 @@ import {UserServiceService} from '../../services/user-service.service';
 import {MessageService} from '../../services/message.service';
 import {Router} from '@angular/router';
 import {LocalStorageService} from '../../services/local-storage.service';
+import {Md5} from 'ts-md5';
 
 @Component({
   selector: 'app-change-password',
@@ -12,6 +13,7 @@ import {LocalStorageService} from '../../services/local-storage.service';
 export class ChangePasswordPage implements OnInit {
 
   change = {
+    oldpassword: '',
     password: '',
     confirmPassword: '',
   }
@@ -19,12 +21,22 @@ export class ChangePasswordPage implements OnInit {
   constructor(private userService: UserServiceService,
               private messageServer: MessageService,
               private router: Router,
-              private localStorage: LocalStorageService) { }
+              private localStorage: LocalStorageService) {}
 
   ngOnInit() {
   }
+
   changePawd() {
     let username = this.localStorage.get('currentUser', 'null');
+    let user = this.userService.getUser();
+    if (user.password !== Md5.hashStr(this.change.oldpassword)) {
+      this.messageServer.alertMessage('警告', '您输入的原密码不正确', 2);
+      return;
+    }
+    if (this.change.password !== this.change.confirmPassword) {
+      this.messageServer.alertMessage('警告', '两次密码不一致', 2);
+      return;
+    }
     this.userService.changePassword(this.change.password, username);
     this.messageServer.toastMessage('修改成功', '2');
     this.router.navigateByUrl('\setting');
