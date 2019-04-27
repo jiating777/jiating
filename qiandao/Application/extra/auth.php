0cbeaaff0801c9034071eb7dfa015257827b0bcc
jiaTing <1210409573@qq.com>
2019-04-27 16:30:46 +0800

    添加丢失文件

--- qiandao/Application/extra/auth.php
+++ qiandao/Application/extra/auth.php
@@ -1,11 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2018/8/6 0006
- * Time: 下午 8:14
- */
+
 return [
     //系统管理员独有的权限
-    'sys_auth' => ['59'],
+    'sys_auth' => [''],
 ];
--- qiandao/Application/extra/status.php
+++ qiandao/Application/extra/status.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2018/4/19 0019
- * Time: 下午 3:44
- */
+
 return [
     //后台API状态码
     'SUCCESS_STATUS' => 1,
